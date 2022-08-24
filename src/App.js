a98831515f2384dae4890276fa3e81d24942919a
Miguel Villanueva <emvil542@gmail.com>
2022-08-24 13:57:22 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__toolbar">
+        <section>
+          <h1>MK Notes</h1>
+        </section>
+
+        {/* dynamic search */}
+        {/* night mode toggle */}
+      </div>
+      <div className="App__main">
+        {/* notes here */}
+      </div>
     </div>
   );
 }
