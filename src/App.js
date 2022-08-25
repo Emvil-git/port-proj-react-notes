3ea27a47c992bc19fca58869c0636558bf42ccd8
Miguel Villanueva <emvil542@gmail.com>
2022-08-25 20:59:55 +0800

    feat: add note component contents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import Note from './components/Note';
 
 function App() {
+  let maxNoteId = appNotes.length;
+  
+  //CONSTANTS
+  const [appNotes, setAppNotes] = useState([]);
+
+  //METHODS
+  // const hasNotes = () => {
+
+  // }
+  
+  const handleAddNotes = () => {
+    const date = new Date();
+
+    appNotes.append(
+      {
+        noteId: maxNoteId,
+        color: '#8ba4f3',
+        title: '',
+        text: "Click the Edit Button to edit notes",
+        date: date.toString().substr(4,11)
+      }
+    )
+  }
+
   return (
     <div className="App">
       <div className="App__toolbar">
@@ -12,9 +36,13 @@ function App() {
 
         {/* dynamic search */}
         {/* night mode toggle */}
+
+        <button>Add Note</button>
       </div>
       <div className="App__main">
+        {appNotes.map(note => <Note />)}
         {/* notes here */}
+        {/* add notes */}
       </div>
     </div>
   );
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,35 @@
-const Note = () => {
+import { useState } from "react";
+
+//CONSTANTS
+const [isStar, setIsStar] = useState(false);
+const [isEditing, setIsEditing] = useState(false);
+
+//METHODS
+
+const starIndicate = () => {
+    if(isStar) return <button></button>;
+    return <button></button>
+    // TODO:
+    // notes star indication function
+}
+
+const Note = ({noteId, title, text, colour, date}) => {
     return(
-        <div></div>
+        <div className="note">
+            <section className="note__header">
+                <h1 className="note__title">{title}</h1>
+                {starIndicate()}
+            </section>
+            <p className="note__text">{text}</p>
+            <section className="note__footer">
+                <span className="note__date">{date}</span>
+                <section className="note__actions">
+                    <button className="note__btn">Edit</button>
+                    <button className="note__btn">Delete</button>
+                </section>
+            </section>
+        </div>
     )
 }
+
+export default Note;
